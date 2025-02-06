195087b40c8359a36c7ec54ba27c083c61031b84
thdus17 <dusghk1717@gmail.com>
2025-02-06 17:49:16 +0900

    add: array 04-05 (05시간초과로 개념 검색해서 다시)

---
+++ src/main/java/org/example/Ct0205.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ct0205 {
+    /**
+     * 5. 소수(에라토스테네스 체)
+     * 설명
+     * 자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
+     * 만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
+     *
+     *
+     * 입력
+     * 첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
+     *
+     * 출력
+     * 첫 줄에 소수의 개수를 출력합니다.
+     *
+     *
+     * 예시 입력 1
+     * 20
+     *
+     * 예시 출력 1
+     * 8
+     */
+    public int solution(int n) { /*자연수의 개수 N(2<=N<=200,000) 소수 -  자기자신과 1로만 나누어지는 수 (1 제외)*/
+        /* 1. 짝수 거르기 (2로 나누어지는 수)
+        * 2. 2,3,5,7
+        * 4.
+        * 5.
+        * */
+        int cnt = 0;
+
+        for (int i = 2 ; i <= n ; i++) {
+            if (isPrime(i)) cnt++;
+        }
+        return cnt;
+    }
+
+    public boolean isPrime(int n) {
+        for (int i=2 ; i*i<=n ;i++) {
+            if (n%i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        Ct0205 main = new Ct0205();
+        System.out.println(main.solution(n));
+    }
+}
