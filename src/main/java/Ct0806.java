c4d2f1cd17cae7e4c46f10172c8c5610aac0e58b
thdus17 <dusghk1717@gmail.com>
2025-03-21 13:57:20 +0900

    add: 0804-0805 dfs 활용 풀이

---
+++ src/main/java/Ct0806.java
@@ -0,0 +1,64 @@
+import java.security.Principal;
+import java.util.Scanner;
+
+public class Ct0806 {
+    /**
+     6. 순열 구하기 - 채점지원X
+     10이하의 N개의 자연수가 주어지면 이 중 M개를 뽑아 일렬로 나열하는 방법을 모두 출력합
+     니다.
+
+     ▣ 입력설명
+     첫 번째 줄에 자연수 N(3<=N<=10)과 M(2<=M<=N) 이 주어집니다.
+     두 번째 줄에 N개의 자연수가 오름차순으로 주어집니다.
+
+     ▣ 출력설명
+     첫 번째 줄에 결과를 출력합니다.
+     출력순서는 사전순으로 오름차순으로 출력합니다.
+
+
+     ▣ 입력예제 1
+     3 2
+     3 6 9
+
+     ▣ 출력예제 1
+     3 6
+     3 9
+     6 3
+     6 9
+     9 3
+     9 6
+
+     */
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] ch;
+
+    public void DFS(int cnt, String answer) {
+        /* M번 뽑아 나열하는 방법 // 출력순서는 사전순으로 오름차순 */
+        if (m==cnt) {
+            System.out.println(answer);
+        } else {
+            for (int i=0; i<n; i++) {
+                if (ch[i]==0) {
+                    ch[i] = 1;
+                    DFS(cnt+1, answer + arr[i]+ " ");
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        ch = new int[n];
+        arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        Ct0806 main = new Ct0806();
+        main.DFS(0, "");
+    }
+}
